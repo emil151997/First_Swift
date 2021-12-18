231208f8401066f9aca27f7ada57fdd5e09ce696
emil151997 <emil151997@mail.ru>
2021-12-18 11:46:26 +0300

    Initial Commit

---
+++ First_Appliacation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First_Appliacation
+//
+//  Created by Emil Minosyan on 18.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Emil Minosyan")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First_Appliacation/First_AppliacationApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_AppliacationApp.swift
+//  First_Appliacation
+//
+//  Created by Emil Minosyan on 18.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct First_AppliacationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
